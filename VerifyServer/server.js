4d5929084fb1ecb295ccdb2cb02da0177f9d2c8a
2022051615270夏瑞涛 <2312609759@qq.com>
2025-04-15 19:34:39 +0800

    VerifyServer:完善验证码服务并使用svg-captcha库实现数字验证码的生成.

--- VerifyServer/server.js
+++ VerifyServer/server.js
@@ -1,28 +1,42 @@
 const grpc = require('@grpc/grpc-js')
-const message_proto = require('./proto')
-const config_module = require('./config')
-const email_module = require('./email')
-const const_module = require('./const')
-const { v4: uuidv4 } = require('uuid')
+const messageProto = require('./proto')
+const configModule = require('./config')
+const emailModule = require('./email')
+const constModule = require('./const')
+const svgCaptcha = require('svg-captcha');
 
 async function GetVerifyCode(call, callback) {
     console.log('email is ', call.request.email)
     try {
-        uniqueId = uuidv4()
+        // 生成验证码
+        const captcha = svgCaptcha.create({
+            size: 6,       // 验证码长度
+            ignoreChars: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', // 忽略字母
+            noise: 2,      // 干扰线数量
+            color: true    // 彩色验证码
+        })
+        uniqueId = captcha.text
         console.log('uniqueId is ', uniqueId)
+
         // 编辑发送Options
+        let emailText = '你已选择此邮箱地址作为你的 ChatRoom 账户。为验证此电子邮件地址属于你，' +
+            '请在电子邮件验证页输入下方验证码：\n\n' + uniqueId + '\n\n此邮件发出三分钟后，验证码过期。\n\n' +
+            '你收到此电子邮件的原因：\n' +
+            'ChatRoom 会在你选择电子邮件地址为 ChatRoom 账户时提出验证要求。你的 ChatRoom 账户需经过验证才能使用。'
         let mailOptions = {
-            from: config_module.email_user,
+            from: configModule.email_user,
             to: call.request.email,
             subject: '验证码',
-            text: '您的验证码为' + uniqueId + '请三分钟内完成注册',
+            text: emailText,
         };
+
         // 阻塞来发送信息
-        let send_res = await email_module.SendMail(mailOptions);
+        let send_res = await emailModule.SendMail(mailOptions);
         console.log("send res is ", send_res)
+
         // 编辑回包
         callback(null, {
-            error: const_module.Errors.Success,
+            error: constModule.Errors.Success,
             email: call.request.email,
             code: uniqueId
         });
@@ -32,16 +46,16 @@ async function GetVerifyCode(call, callback) {
         // 编辑回包
         callback(null, {
             email: call.request.email,
-            error: const_module.Errors.Exception
+            error: constModule.Errors.Exception
         });
     }
 }
 
 function main() {
     // 创建GRPC服务器
-    var server = new grpc.Server()
+    let server = new grpc.Server()
     // 为服务器添加服务
-    server.addService(message_proto.VerifyService.service, { GetVerifyCode: GetVerifyCode })
+    server.addService(messageProto.VerifyService.service, { GetVerifyCode: GetVerifyCode })
     // 开启服务
     server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
         console.log(`grpc server started, port is ${port}`)
