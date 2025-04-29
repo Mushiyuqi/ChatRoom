534c24c171ef88526869ed2c7c5c54cd2ba6baad
2022051615270夏瑞涛 <2312609759@qq.com>
2025-04-29 20:13:40 +0800

    VerifyServer: 发送验证码时添加过期功能在Redis中.

--- VerifyServer/const.js
+++ VerifyServer/const.js
@@ -1,9 +1,12 @@
+const { SendMail } = require("./email");
+
 let code_prefix = "code_";
 
 const Errors = {
   Success: 0,
   RedisErr: 1,
   Exception: 2,
+  SendMailErr: 3,
 };
 
 
--- VerifyServer/server.js
+++ VerifyServer/server.js
@@ -4,20 +4,37 @@ const configModule = require('./config')
 const emailModule = require('./email')
 const constModule = require('./const')
 const svgCaptcha = require('svg-captcha');
+const redisModule = require('./redis')
 
 async function GetVerifyCode(call, callback) {
     console.log('email is ', call.request.email)
     try {
-        // 生成验证码
-        const captcha = svgCaptcha.create({
-            size: 6,       // 验证码长度
-            ignoreChars: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', // 忽略字母
-            noise: 2,      // 干扰线数量
-            color: true    // 彩色验证码
-        })
-        uniqueId = captcha.text
-        console.log('uniqueId is ', uniqueId)
-
+        // 查询该邮箱是否已存在
+        let queryRes = await redisModule.GetRedis(constModule.code_prefix + call.request.email)
+        console.log('queryRes is ', queryRes)
+        // 获取之前发送过的验证码
+        let uniqueId = queryRes;
+        if (queryRes == null) {
+            // 不存在则 生成验证码
+            const captcha = svgCaptcha.create({
+                size: 6,       // 验证码长度
+                ignoreChars: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', // 忽略字母
+                noise: 2,      // 干扰线数量
+                color: true    // 彩色验证码
+            })
+            uniqueId = captcha.text
+            console.log('uniqueId is ', uniqueId)
+            // 将验证码保存到redis中 三分钟过期
+            let bres = await redisModule.SetRedisExpire(constModule.code_prefix + call.request.email, uniqueId, 360)
+            if (!bres) {
+                // redis 操作失败 编辑回包
+                callback(null, {
+                    error: constModule.Errors.RedisErr,
+                    email: call.request.email,
+                });
+                return
+            }
+        }
         // 编辑发送的文本
         let emailText = '你已选择此邮箱地址作为你的 ChatRoom 账户。为验证此电子邮件地址属于你，' +
             '请在电子邮件验证页输入下方验证码：\n\n' + uniqueId + '\n\n此邮件发出三分钟后，验证码过期。\n\n' +
@@ -32,8 +49,16 @@ async function GetVerifyCode(call, callback) {
         };
 
         // 阻塞来发送信息
-        let send_res = await emailModule.SendMail(mailOptions);
-        console.log("send res is ", send_res)
+        let sendRes = await emailModule.SendMail(mailOptions);
+        console.log("send res is ", sendRes)
+        if (!sendRes) {
+            // SendMail 操作失败 编辑回包
+            callback(null, {
+                error: constModule.Errors.SendMailErr,
+                email: call.request.email,
+            });
+            return
+        }
 
         // 编辑回包
         callback(null, {
