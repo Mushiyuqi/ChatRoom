91b8d90cff5d9c70ebc8771c0d20c8a399579014
2022051615270夏瑞涛 <2312609759@qq.com>
2025-06-10 21:18:09 +0800

    chatRoom: 完成search栏外观设计.

--- ChatRoom/chatdialog.cpp
+++ ChatRoom/chatdialog.cpp
@@ -1,5 +1,6 @@
 #include "chatdialog.h"
 #include "ui_chatdialog.h"
+#include <QAction>
 
 ChatDialog::ChatDialog(QWidget *parent)
     : QDialog(parent)
@@ -9,6 +10,29 @@ ChatDialog::ChatDialog(QWidget *parent)
     // 设置add按钮样式
     ui->add_btn->SetState("normal", "hover", "press");
     ui->add_btn->setProperty("state", "normal");
+
+    // 设置search_edit
+    ui->search_edit->SetMaxLength(15);
+
+    QAction* searchAction = new QAction(ui->search_edit);
+    searchAction->setIcon(QIcon(":/resource/search.png"));
+    ui->search_edit->addAction(searchAction, QLineEdit::LeadingPosition);
+    ui->search_edit->setPlaceholderText(QStringLiteral("搜索"));
+
+    QAction* clearAction = new QAction(ui->search_edit);
+    clearAction->setIcon(QIcon(":/resource/close_transparent.png"));
+    ui->search_edit->addAction(clearAction, QLineEdit::TrailingPosition);
+    connect(ui->search_edit, &QLineEdit::textChanged, [clearAction](const QString &text){
+        if(!text.isEmpty()){
+            clearAction->setIcon(QIcon(":/resource/close_search.png"));
+        }else{
+            clearAction->setIcon(QIcon(":/resource/close_transparent.png"));
+        }
+    });
+    connect(clearAction, &QAction::triggered, [this](){
+        ui->search_edit->clear();
+    });
+
 }
 
 ChatDialog::~ChatDialog()
