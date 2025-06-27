3c9a857f617ccd9ad6ab761a56e5548221b1f460
2022051615270夏瑞涛 <2312609759@qq.com>
2025-06-27 10:00:56 +0800

    ChatServer: 完成基础添加好友申请分布式接收逻辑.

--- ChatServer2/ChatServiceImpl.cpp
+++ ChatServer2/ChatServiceImpl.cpp
@@ -10,6 +10,32 @@ ChatServiceImpl::ChatServiceImpl() {
 }
 
 Status ChatServiceImpl::NotifyAddFriend(ServerContext* context, const AddFriendReq* request, AddFriendRsp* reply) {
+    auto touid = request->touid();
+    auto session = UserManager::GetInstance().GetSession(touid);
+
+    Defer defer([request, reply]() {
+        reply->set_error(ErrorCodes::Success);
+        reply->set_applyuid(request->applyuid());
+        reply->set_touid(request->touid());
+    });
+
+    // 用户不在内存中直接返回 todo
+    if(session == nullptr) {
+        return Status::OK;
+    }
+
+    // 在内存中则直接发送通知对方
+    Json::Value returnValue;
+    returnValue["error"] = ErrorCodes::Success;;
+    returnValue["applyuid"] = request->applyuid();
+    returnValue["name"] = request->name();
+    returnValue["desc"] = request->desc();
+    returnValue["icon"] = request->icon();
+    returnValue["sex"] = request->sex();
+    returnValue["nick"] = request->nick();
+
+    session->Send(returnValue.toStyledString(), ReqId::ID_NOTIFY_ADD_FRIEND);
+
     return Status::OK;
 }
 
